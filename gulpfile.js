42ca421f3a86149aa15d072e7fffaf092aac5615
joseolinda <joseolindad@yahoo.com.br>
2023-01-18 15:50:32 -0300

    Add watch

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 
-function buildStyles() {
-  return gulp.src('./src/scss/**/*.scss')
+gulp.task('buildStyles', () => {
+  return gulp.src('./src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist/css'));
-};
+});
 
-function icons() {
+gulp.task('icons', () => {
     return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
         .pipe(gulp.dest('./dist/assets/webfonts/'));
-};
+});
  
 gulp.task('minify-css', () => {
   return gulp.src('./dist/css/*.css')
@@ -21,5 +21,8 @@ gulp.task('minify-css', () => {
     .pipe(gulp.dest('./dist/css'));
 });
 
-exports.buildStyles = buildStyles;
-exports.icons = icons;
+gulp.task('watch', () => {
+  gulp.watch('./src/scss/**/*.scss', (done) => {
+      gulp.series(['buildStyles', 'minify-css'])(done);
+  });
+});
