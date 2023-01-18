b9a38ae66a1eddfec861e7eedfdd0aa2f9e38dca
joseolinda <joseolindad@yahoo.com.br>
2023-01-18 15:35:51 -0300

    Uso de gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+
+function buildStyles() {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+};
+
+function icons() {
+    return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('./dist/assets/webfonts/'));
+};
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('./dist/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+exports.buildStyles = buildStyles;
+exports.icons = icons;
